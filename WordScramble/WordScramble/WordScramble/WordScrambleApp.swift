a00901055b6a8a0e466db4d41b9003dd43c0c6e0
Alayoubi,Ranim R <r.alayoubi@student.fontys.nl>
2021-04-16 13:15:27 +0200

    Simple swiftui WordScrumble simple app is implemented

---
+++ WordScramble/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Ranim Alayoubi on 26/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
